b0329fdb4769c8b34213cb39d819992aaed2f643
Thierry Girod <thierry.girod@hotmail.com>
2022-11-03 13:44:09 +0100

    Add file loop for processing

--- 2_preprocessing.py
+++ 2_preprocessing.py
@@ -9,49 +9,51 @@ def preprocess():
 
     for corpusFile in CorpusApi.getCorpusFileByFile():
         files.append(corpusFile)
+
+    for file in files:
     
-    with open(files[1], 'r') as jsonFile:
+        with open(file, 'r') as jsonFile:
 
-        fileName = files[1].split('/')[-1]
-        print(f'Start processing {fileName}')
+            fileName = file.split('/')[-1]
+            print(f'Start processing {fileName}')
 
-        # Load the documents
-        lines = jsonFile.readlines()
+            # Load the documents
+            lines = jsonFile.readlines()
 
-        # Load the documents into list
-        documents = []
-        for line in lines:
-            document = json.loads(line)
-            documents.append(document)
+            # Load the documents into list
+            documents = []
+            for line in lines:
+                document = json.loads(line)
+                documents.append(document)
 
-        # Preprocess the documents
-        for i, document in enumerate(documents):
-            # replace linebreaks with whitespaces
-            document['title'] = re.sub('\n', ' ', document['title'])
-            document['headings'] = re.sub('\n', ' ', document['headings'])
-            document['body'] = re.sub('\n', ' ', document['body'])
+            # Preprocess the documents
+            for i, document in enumerate(documents):
+                # replace linebreaks with whitespaces
+                document['title'] = re.sub('\n', ' ', document['title'])
+                document['headings'] = re.sub('\n', ' ', document['headings'])
+                document['body'] = re.sub('\n', ' ', document['body'])
 
-            # replace punctuations
-            document['title'] = re.sub('[^\w\s]', '', document['title'])
-            document['headings'] = re.sub('[^\w\s]', '', document['headings'])
-            document['body'] = re.sub('[^\w\s]', '', document['body'])
+                # replace punctuations
+                document['title'] = re.sub('[^\w\s]', '', document['title'])
+                document['headings'] = re.sub('[^\w\s]', '', document['headings'])
+                document['body'] = re.sub('[^\w\s]', '', document['body'])
 
-            # lower casing
-            document['title'] = document['title'].lower()
-            document['headings'] = document['headings'].lower()
-            document['body'] = document['body'].lower()
+                # lower casing
+                document['title'] = document['title'].lower()
+                document['headings'] = document['headings'].lower()
+                document['body'] = document['body'].lower()
 
-            # replace multiple whitespaces with only one
-            # must be at the end of the preprocessing step
-            document['title'] = re.sub('\s{1,}', ' ', document['title'])
-            document['headings'] = re.sub('\s{1,}', ' ', document['headings'])
-            document['body'] = re.sub('\s{1,}', ' ', document['body'])
+                # replace multiple whitespaces with only one
+                # must be at the end of the preprocessing step
+                document['title'] = re.sub('\s{1,}', ' ', document['title'])
+                document['headings'] = re.sub('\s{1,}', ' ', document['headings'])
+                document['body'] = re.sub('\s{1,}', ' ', document['body'])
 
-            print(f'{fileName}:{i+1}/{len(documents)} processed')
+                print(f'{fileName}:{i+1}/{len(documents)} processed')
+            
+            CorpusApi.saveListAsJson(f'{Config.CORPUS_ROOT_DIRECTORY}/processed/{fileName}_processed.json', documents)
+            print(f'End processing {fileName}')
         
-        CorpusApi.saveListAsJson(f'{Config.CORPUS_ROOT_DIRECTORY}/processed/{fileName}_processed.json', documents)
-        print(f'End processing {fileName}')
-    
 
 if __name__ == '__main__':
     preprocess()
