6cb41d1b8fe67f45d07e67b9718fd8e178f4cbcf
Thierry Girod <thierry.girod@hotmail.com>
2022-11-03 12:01:21 +0100

    Update config with corpus root

--- ltr/data/Config.py
+++ ltr/data/Config.py
@@ -2,4 +2,5 @@
 DATA_DIRECTORY = '/home/ubuntu/trec-ltr-thesis/data'
 DEV1_QUERIES_TSV = f'{DATA_DIRECTORY}/dev1/docv2_dev_queries.tsv'
 DEV1_QRELS_TSV = f'{DATA_DIRECTORY}/dev1/docv2_dev_qrels.tsv'
-CORPUS_DIRECTORY = f'{DATA_DIRECTORY}/corpus/msmarco_v2_doc'
+CORPUS_ROOT_DIRECTORY = f'{DATA_DIRECTORY}/corpus'
+CORPUS_DIRECTORY = f'{CORPUS_ROOT_DIRECTORY}/msmarco_v2_doc'
