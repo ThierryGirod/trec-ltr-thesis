af2cf25cdbede8a2bf5505edcd4a1ad772e477a3
Thierry Girod <thierry.girod@hotmail.com>
2022-11-08 14:21:30 +0100

    Indexing update

---
+++ 3_indexing.py
@@ -0,0 +1,17 @@
+from solr import Solr
+from ltr.data import CorpusApi
+import json
+import requests
+
+collectionName = 'thesis-ltr'
+Solr.createCollection(collectionName)
+Solr.enableLtr(collectionName)
+
+Solr.createTextField(collectionName, 'id')
+Solr.createTextField(collectionName, 'url')
+Solr.createTextField(collectionName, 'title')
+Solr.createTextField(collectionName, 'headings')
+Solr.createTextField(collectionName, 'body')
+
+for file in CorpusApi.getCorpusFileByFile(processed = True):    
+    Solr.indexFile(collectionName, file)
