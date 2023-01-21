171d06246330fc8f1c19d3d897cc472e855810f6
Thierry Girod <thierry.girod@hotmail.com>
2023-01-21 18:19:19 +0100

    stop deleting files

--- 3_indexing.py
+++ 3_indexing.py
@@ -5,26 +5,24 @@ import requests
 import time
 
 def indexing():
+    try: 
+        collectionName = 'thesis-ltr'
+        typeName = 'custom_text_general'
+        Solr.createTextType(collectionName, typeName)
+        Solr.createTextField(collectionName, 'url', typeName)
+        Solr.createTextField(collectionName, 'title', typeName)
+        Solr.createTextField(collectionName, 'headings', typeName)
+        Solr.createTextField(collectionName, 'body', typeName)
+        Solr.addCopyField(collectionName, '*', '_text_')
 
-    collectionName = 'thesis-ltr'
-    Solr.createCollection(collectionName)
-    Solr.enableLtr(collectionName)
-    
-    #Solr.createTextField(collectionName, 'docid')
-    typeName = 'custom_text_general'
-    Solr.createTextType(collectionName, typeName)
-    Solr.createTextField(collectionName, 'url', typeName)
-    Solr.createTextField(collectionName, 'title', typeName)
-    Solr.createTextField(collectionName, 'headings', typeName)
-    Solr.createTextField(collectionName, 'body', typeName)
-    Solr.addCopyField(collectionName, '*', '_text_')
-
-    for file in CorpusApi.getCorpusFileByFile(processed = True):
-        start = time.time()    
-        Solr.indexFile(collectionName, file)
-        #CorpusApi.deleteFile(file)
-        end = time.time()
-        print('Time taken for indexing file:', end - start)
+        for file in CorpusApi.getCorpusFileByFile(processed = True):
+            start = time.time()    
+            Solr.indexFile(collectionName, file)
+            #CorpusApi.deleteFile(file)
+            end = time.time()
+            print('Time taken for indexing file:', end - start)
+    except Exception as e:
+        print(e)
         
 if __name__ == '__main__':
     indexing()
