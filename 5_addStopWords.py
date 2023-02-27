34c91d6cc10df15e3c862b285bdcb59359497b30
Thierry Girod <thierry.girod@hotmail.com>
2023-02-27 15:51:37 +0100

    Rename and comment stop words

--- 5_addStopWords.py
+++ 5_addStopWords.py
@@ -2,7 +2,7 @@ from solr import Solr
 import nltk
 from nltk.corpus import stopwords
 
-
+# Get the stopwords from nltk and import them to the collection
 def addStopWords():
     collectionName = 'thesis-ltr'
     language = 'english'
